c62e49d6819d4c85537501e64e2bc0227358963f
marcosdary <141282180+marcosdary@users.noreply.github.com>
2024-04-04 20:42:26 -0300

    Passagem área
    Faz a escrita dos dados pela empresa para depois ser armazenado pelo cliente para reservar os assentos disponíveis

--- ex019.py
+++ ex019.py
@@ -1,17 +1,23 @@
-class Solution:
-    
-    def reverse(self, x: int) -> int:
+# Sistema de Reserva de Passagens Aéreas
+
+class PassagemArea:
+    def __init__(self, origem, destino, data, assentos_disponiveis) -> None:
+        self.origem = origem
+        self.destino = destino
+        self.data = data
+        self.assentos_disponiveis = assentos_disponiveis
         
-        k = 1 if x >= 0 else -1
-        r = int(''.join(reversed(abs(x))))*k
-        
-        return r if -2**31 <= r <= 2**31-1 else 0
+    def reserva_assento(self, quantidade):
+        if quantidade <= self.assentos_disponiveis:
+            self.assentos_disponiveis -= quantidade
+            return f'Assento reservado com sucesso\nA quantidade de assentos igual a {quantidade}'
+        else:
+            return f'Infelizmente, a quantidade está acima dos assentos disponíveis'
     
-n = -123
-p = Solution()
-print(p.reverse(n))
-
-
-
-
-
+    def __str__(self):
+        return f'Detalhes\n\tOrigem: {self.origem}\n\tDestino: {self.destino}\n\tData: {self.data}\n\tAssentos Disponíveis: {self.assentos_disponiveis}'
+        
+latam = PassagemArea('São Paulo', 'Estados Unidos', "12/10/2025", 100)
+print(latam.reserva_assento(8))
+print(latam)
+    
