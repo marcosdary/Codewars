4dcdc7902e28ae59620c607bd6ef7ffa83a7bc3c
marcosdary <141282180+marcosdary@users.noreply.github.com>
2024-06-24 17:07:29 -0300

    Banco

--- ex071.py
+++ ex071.py
@@ -1,16 +1,25 @@
-from math import sqrt
-
-def is_prime(n):
-    if n <= 1:
-        return False
-    for i in range(2, int(sqrt(n))+1):
-        if n % i == 0:
+class BankAccount:
+    def __init__(self, deposit, fees, inflation) -> None:
+        self.deposit = deposit
+        self.fees = fees
+        self.inflation = inflation
+    
+    def lifeplan(self, value, time):
+        if self.deposit >= 0 and time > 0:
+            return True
+        
+        if self.deposit < 0:
             return False
-    return True
+        cal = self.deposit + ((self.fees/100)*self.deposit) - value
+        self.deposit = round(cal, 0)
+        value = round(value + (value * (self.inflation/100)), 1)
+        
+        time -= 1
 
-def twin_prime(n):
-    s = 0
-    for i in range(1, n):
-        if is_prime(i) and is_prime(i+2):
-            s += 1   
-    return s
+        return self.lifeplan(value, time)
+    
+def fortune(f0, p, c0, n, i):
+    account = BankAccount(f0, p, i)
+    return account.lifeplan(c0, n)
+
+print(fortune(4383861, 4, 293380, 14, 7))
