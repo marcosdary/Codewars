f55e106213eeaf1ad8eb33546270b6aa3d4a2a73
marcosdary <141282180+marcosdary@users.noreply.github.com>
2024-08-09 10:22:58 -0300

    Hola!!!

---
+++ ex150.py
@@ -0,0 +1,4 @@
+# Implemente o jogo dos oito em Python, onde o jogador deve ordenar os números de 1 a 8 em uma matriz 3x3, 
+# movendo um espaço vazio (representado por 0). O programa deve permitir ao usuário escolher os movimentos 
+# e mostrar o estado atual do tabuleiro após cada movimento
+
